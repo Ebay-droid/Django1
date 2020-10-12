f24ede7972aad52dd805ab26521a5c194327aa20
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-12 12:08:03 +0300

    media settings added

--- ebay/urls.py
+++ ebay/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
   path('',views.index,name='index'),
   path('search/',views.search_results,name='search_results'),
-  path('images/<id>',views.get_image_by_id, name ='get_image_by_id')
+  path('images/<int:id>',views.get_image_by_id, name ='get_image_by_id')
 ]
