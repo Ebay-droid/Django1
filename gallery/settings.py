8205897dff9c4386250d01a5a6c314d65d017b76
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-10-13 10:57:51 +0300

    deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL= '/home/ebay/Python/Django/my-gallery/media/images'
-MEDIA_ROOT = os.path.join(BASE_DIR,'/home/ebay/Python/Django/my-gallery/media/images')
+MEDIA_URL= '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 django_heroku.settings(locals())
